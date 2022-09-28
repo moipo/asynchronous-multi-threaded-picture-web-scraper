f666ad5359c60c32ac8fdec5c1f5f0678efee3ae
moipo <moipochtoviyyashik@inbox.ru>
2022-09-28 11:38:08 +0300

    set the amount of pictures + os operations

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,4 +1,7 @@
 from parsing.mtpws import start
 
 
-start()
+#validationerror
+
+img_qua = int(input("Input the amount of pictures you'd like to download:"))
+if type(img_qua) == int: start(img_qua)
--- web_scraper/parsing/mtpws.py
+++ web_scraper/parsing/mtpws.py
@@ -1,21 +1,30 @@
 from .parser_1 import Parser1
 from .parser_2 import Parser2
 from threading import *
+import os
+import pathlib
 
+def start(img_qua):
 
-def start():
+    pictures_path = os.path.join(pathlib.Path(__file__).parent.parent.resolve(), "pictures")
+    # os.rmdir(pictures_path)
+    # os.mkdir(pictures_path)
+    os.startfile(pictures_path)
 
-    p1 = Parser1()
-    p2 = Parser2()
-
-    t1 = Thread(target=p1.parse_pages_in_range,args = (2,5))
-    t2 = Thread(target=p2.parse_pages_in_range,args = (2,5))
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
+    #
+    # p1 = Parser1()
+    # p2 = Parser2()
+    #
+    # quantity = {"img_qua" : img_qua}
+    #
+    # t1 = Thread(target=p1.parse_pages_in_range,args = (1,1000,quantity))
+    # t2 = Thread(target=p2.parse_pages_in_range,args = (1,1000,quantity))
+    #
+    # t1.start()
+    # t2.start()
+    #
+    # t1.join()
+    # t2.join()
 
 
 
